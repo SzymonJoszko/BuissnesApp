b61ecd812dbb689ef18af0ca3c5c371de95bd19f
Szymek <szymonjoszko@gmail.com>
2020-01-22 00:23:40 +0100

    Bugfix - state after rotation

--- app/src/main/java/com/example/businessapp/MainActivity.kt
+++ app/src/main/java/com/example/businessapp/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.businessapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.example.businessapp.Fragments.RecyclerFragment.AllCompaniesFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val tagAllCompaniesFragment :String = "ALL_COMPANIES_FRAGMENT"
+    private var currentFragment :String = tagAllCompaniesFragment
+    private val currentTagFragmentKey :String = "FRAGMENT_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +25,31 @@ class MainActivity : AppCompatActivity() {
         // tworzenie fragment managera
         val frgmentManager = supportFragmentManager
 
-        // dodawnaie fragmentu do main activity
-        frgmentManager.beginTransaction().add(R.id.fragment_container, allCompaniesFragment).commit()
+        // przypisywanie obecnego fragmentu
+        if(savedInstanceState != null){
+            currentFragment = savedInstanceState.getString(currentTagFragmentKey)!!
+        }
 
+        // dodawnaie obecnego fragmentu do main activity
+        if(currentFragment == tagAllCompaniesFragment) {
+            frgmentManager.beginTransaction()
+                .replace(R.id.fragment_container, allCompaniesFragment, currentFragment).commit()
+        }else{
+            frgmentManager.beginTransaction().replace(R.id.fragment_container, frgmentManager.findFragmentByTag(currentFragment) as Fragment, currentFragment).commit()
+        }
     }
+
+    // setter currentFragmentu
+    fun setMyCurrentFragment(tag:String){
+        currentFragment = tag
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(currentTagFragmentKey, currentFragment)
+    }
+
 
     // tymczasowe rozwiazanie do wracania
     override fun onBackPressed() {
@@ -29,6 +59,8 @@ class MainActivity : AppCompatActivity() {
             AllCompaniesFragment()
         // tworzenie fragment managera
         val frgmentManager = supportFragmentManager
+        // ustawianei obecnego fragmentu
+        setMyCurrentFragment(tagAllCompaniesFragment)
 
         // zamiana fragmentu
         frgmentManager.beginTransaction().replace(R.id.fragment_container, allCompaniesFragment).commit()
